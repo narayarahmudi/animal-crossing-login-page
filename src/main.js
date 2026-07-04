34f4c72017519114623e0f8f5593e872e3f7afd3
Dava Naraya Rahmudi <naraya.developer.js@gmail.com>
2026-07-05 00:14:30 +0700

    Login page - Animal Crossing v1

---
+++ src/main.js
@@ -0,0 +1,7 @@
+const switcher = document.getElementById("switch")
+const circle = document.getElementById("circle")
+
+switcher.addEventListener("click", () => {
+    // circle.classList.toggle("switch-on")
+    switcher.classList.toggle("active")
+})
